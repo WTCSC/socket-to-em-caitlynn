c734434e3672dc83632d11c257312b5caf3685bc
Kat <cpnguyen0000@gmail.com>
2025-02-19 13:10:29 -0700

    Did some stuff for it

--- server.py
+++ server.py
@@ -24,5 +24,87 @@ color_codes = {
     "cyan": "\033[38;5;51m",
     "light blue": "\033[38;5;81m",
     "blue": "\033[38;5;21m",
-    "purple": "\033[38;5;93m"
+    "purple": "\033[38;5;93m",
+    "reset": "\033[0m"
 }
+
+# "Broadcasts" aka sends a message to the sender's room
+def broadcast(message, senders_client):
+    senders_room = client_rooms.get(senders_client, 'public')
+
+    if senders_room == 'public':
+        for client in rooms['public']:
+            if client != senders_client:
+                try:
+                    client.send(message)
+                except:
+                    remove_client(client)
+    
+    elif senders_room in rooms:
+        for client in rooms[senders_room]:
+            if client != senders_client:
+                try:
+                    client.send(message)
+                except:
+                    remove_client(client)
+
+
+# Should a user choose to leave this will remove them from their current room, as well as all structures
+def remove_client(client):
+    for client in clients:
+        index = clients.index(client)
+        username = usernames.index
+        clients.remove(client)
+        usernames.pop(index)
+        colors.pop(index)
+        
+        room = client_rooms.get(client, 'public')
+        if room in rooms and client in rooms[room]:
+            rooms[room].remove(client)
+            broadcast(f"{username} has left the chat.".encode('utf-8'), client)
+
+        del client_rooms[client]
+        client.close
+        print(f"{username} has disconnected.")
+
+
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+
+        # Handels if the client wants to join a room using /join
+            if message.startswith("/join "):
+                room_name = message.split(" ", 1)[1].strip()
+                join_room(client, room_name)
+
+            # Handels if the client wants to leave a room using /leave
+            elif message == ("/leave"):
+                leave_room(client)
+            
+            # Handels if the client wants to exit a room using /exit
+            elif message == ("/exit"):
+                remove_client(client)
+                break
+
+            else:
+                username = usernames[clients.index(client)]
+                color = colors[client.index(client)]
+                colored_message = f"{color}{username}: {message}{color_codes['reset']}"
+                broadcast(colored_message.encode('utf-8'), client)
+
+        except:
+            remove_client(client)
+            break
+
+def join_room(client):
+    print()
+
+def leave_room(client):
+    print()
+
+def choose_color(client):
+    print()
+
+def start_server():
+    print()
